963c656dd67f76a86cf8b99a7701cbbb3cbaeed0
Micha Alt <mail@lean-stack.de>
2021-07-21 22:31:09 +0200

    Install express and refactor app to be an express app.

--- src/app/index.js
+++ src/app/index.js
@@ -1,7 +1,9 @@
-const http = require('http');
+const express = require('express');
 
-const app = http.createServer((req, res) => {
-  res.end('<h1>Node Boilerplate</h1>');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('<h1>Express Web API</h1>');
 });
 
 module.exports = app;
